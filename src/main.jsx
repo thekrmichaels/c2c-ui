c0abf37ea0559352a6742af9f46ca3f4bd93ff6b
Amilkar Díaz <amilkarjdiazh@outlook.com>
2024-03-05 14:34:07 -0500

    build: :heavy_plus_sign: install auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
+const base = "c2c-ui";
 const router = createBrowserRouter([
   {
-    path: "c2c-ui",
+    path: base,
     element: <App />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0Provider
+      domain={import.meta.env.VITE_DOMAIN}
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/${base}/`,
+      }}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
   </React.StrictMode>
 );
