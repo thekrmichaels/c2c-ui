b5d9b3d91001d394584d7883a9dd82768acc725f
Amilkar Díaz <amilkarjdiazh@outlook.com>
2024-03-08 08:55:36 -0500

    chore: auth0

---
+++ src/components/Auth0/LogoutButton.jsx
@@ -0,0 +1,19 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button
+      onClick={() =>
+        logout({
+          logoutParams: { returnTo: `${window.location.origin}/c2c-ui/` },
+        })
+      }
+    >
+      Log Out
+    </button>
+  );
+};
+
+export default LogoutButton;
